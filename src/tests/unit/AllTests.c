e1cf66d6af6bea7acaf970c34d5a2d39bbe84a11
Dean Hall <dwhall256@gmail.com>
2006-10-03 20:27:04 +0000

    #48: added Testing doc and CuTest framework files.

---
+++ src/tests/unit/AllTests.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "CuTest.h"
+
+CuSuite* getTestDictSuite();
+
+void RunAllTests(void)
+{
+    CuString *output = CuStringNew();
+    CuSuite* suite = CuSuiteNew();
+
+    CuSuiteAddSuite(suite, getTestDictSuite());
+
+    CuSuiteRun(suite);
+    CuSuiteSummary(suite, output);
+    CuSuiteDetails(suite, output);
+    printf("%s\n", output->buffer);
+}
+
+int main(void)
+{
+    RunAllTests();
+}

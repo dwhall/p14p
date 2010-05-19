b6d62b4ce474ee5f218f2198a30625694b72396b
Dean Hall <dwhall256@gmail.com>
2010-05-19 21:49:37 +0000

    Issue #86 check in on branch

--- src/platform/econotag/plat.c
+++ src/platform/econotag/plat.c
@@ -12,27 +12,28 @@
 
 #include <stdio.h>
 
+#include <mc1322x.h>
+/*#include <board.h>*/
+
 #include "pm.h"
+
+
+/* Baud rate: 115200 bps, INC=767, MOD=9999, 24Mhz 8x samp */
+#define INC 767
+#define MOD 9999
+#define SAMP UCON_SAMP_8X
 
 
 PmReturn_t
 plat_init(void)
 {
 
-#ifdef DEBUG
-    debug();
-#endif
+	uart1_init(INC,MOD,SAMP);
 
     return PM_RET_OK;
 }
 
-int fputc(int ch, FILE *f)
-{
-    plat_putByte((uint8_t) ch);
-    return ch;
-}
 
-/* TODO: disable the peripherals and interrupts */
 PmReturn_t
 plat_deinit(void)
 {
@@ -47,20 +48,41 @@ plat_deinit(void)
 uint8_t
 plat_memGetByte(PmMemSpace_t memspace, uint8_t const **paddr)
 {
-	return 0;
+    uint8_t b = 0;
+
+    switch (memspace)
+    {
+        case MEMSPACE_RAM:
+        case MEMSPACE_PROG:
+          b = **paddr;
+          *paddr += 1;
+          return b;
+        case MEMSPACE_EEPROM:
+        case MEMSPACE_SEEPROM:
+        case MEMSPACE_OTHER0:
+        case MEMSPACE_OTHER1:
+        case MEMSPACE_OTHER2:
+        case MEMSPACE_OTHER3:
+        default:
+            return 0;
+    }
 }
 
 
 PmReturn_t
 plat_getByte(uint8_t *b)
 {
-	return 0;
+    *b = uart1_getc();
+
+    return PM_RET_OK;
 }
 
 
 PmReturn_t
 plat_putByte(uint8_t b)
 {
+    uart1_putc(b);
+
     return PM_RET_OK;
 }
 
@@ -68,6 +90,8 @@ plat_putByte(uint8_t b)
 PmReturn_t
 plat_getMsTicks(uint32_t *r_ticks)
 {
+    *r_ticks = pm_timerMsTicks;
+
     return PM_RET_OK;
 }
 
@@ -75,5 +99,56 @@ plat_getMsTicks(uint32_t *r_ticks)
 void
 plat_reportError(PmReturn_t result)
 {
+    /* Print error */
+    printf("Error:     0x%02X\n", result);
+    printf("  Release: 0x%02X\n", gVmGlobal.errVmRelease);
+    printf("  FileId:  0x%02X\n", gVmGlobal.errFileId);
+    printf("  LineNum: %d\n", gVmGlobal.errLineNum);
 
+    /* Print traceback */
+    {
+        pPmObj_t pframe;
+        pPmObj_t pstr;
+        PmReturn_t retval;
+
+        printf("Traceback (top first):\n");
+
+        /* Get the top frame */
+        pframe = (pPmObj_t)gVmGlobal.pthread->pframe;
+
+        /* If it's the native frame, print the native function name */
+        if (pframe == (pPmObj_t)&(gVmGlobal.nativeframe))
+        {
+
+            /* The last name in the names tuple of the code obj is the name */
+            retval = tuple_getItem((pPmObj_t)gVmGlobal.nativeframe.nf_func->
+                                   f_co->co_names, -1, &pstr);
+            if ((retval) != PM_RET_OK)
+            {
+                printf("  Unable to get native func name.\n");
+                return;
+            }
+            else
+            {
+                printf("  %s() __NATIVE__\n", ((pPmString_t)pstr)->val);
+            }
+
+            /* Get the frame that called the native frame */
+            pframe = (pPmObj_t)gVmGlobal.nativeframe.nf_back;
+        }
+
+        /* Print the remaining frame stack */
+        for (;
+             pframe != C_NULL;
+             pframe = (pPmObj_t)((pPmFrame_t)pframe)->fo_back)
+        {
+            /* The last name in the names tuple of the code obj is the name */
+            retval = tuple_getItem((pPmObj_t)((pPmFrame_t)pframe)->
+                                   fo_func->f_co->co_names, -1, &pstr);
+            if ((retval) != PM_RET_OK) break;
+
+            printf("  %s()\n", ((pPmString_t)pstr)->val);
+        }
+        printf("  <module>.\n");
+    }
 }

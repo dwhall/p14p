25b6b607a0e6b61d525ed17389225e9256913b84
Dean Hall <dwhall256@gmail.com>
2012-02-20 11:23:28 -0700

    Fixes issue 229 - Added LED1-4 to the pin lookup table on mbed platform

--- src/platform/mbed/mbed.py
+++ src/platform/mbed/mbed.py
@@ -22,7 +22,7 @@ static DigitalOut led4(LED4);
 
 /* PinName lookup table.  Converts pin number to PinName. */
 static PinName const pinNumToName[] = {
-    NC, NC, NC, NC, NC, p5, p6, p7, p8, p9,
+    NC, LED1, LED2, LED3, LED4, p5, p6, p7, p8, p9,
     p10, p11, p12, p13, p14, p15, p16, p17, p18, p19,
     p20, p21, p22, p23, p24, p25, p26, p27, p28, p29,
     p30
